fe74daf8cc2273aad35934fd6d7c937ee882307f
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-03-16 15:30:15 -0700

    all done!

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ * main - Write a C program that prints exactlyl
+ * and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error
+ *
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write();
+	return (0);
+}
