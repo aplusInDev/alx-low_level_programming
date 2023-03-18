d7af454552f89bdd6019693c90c7778b4912a3f5
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-03-18 13:09:42 -0700

    task 12 has been edited!

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,25 +7,30 @@
  */
 int main(void)
 {
-	int d1, d2, d3, d4;
+	int d1, d2;
 
-	for (d1 = 48; d1 <= 57; d1++)
-		for (d2 = 48; d2 <= 57; d2++)
-			for (d3 = d1 ; d3 <= 57; d3++)
-				for (d4 = d2; d4 <= 57; d4++)
+	while (d1 <= 99)
+	{
+		d2 = d1;
+		while (d2 <= 99)
+		{
+			if (d2 != d1)
+			{
+				putchar((d1 / 10) + 48);
+				putchar((d1 % 10) + 48);
+				putchar(' ');
+				putchar((d2 / 10) + 48);
+				putchar((d2 % 10) + 48);
+				if (d1 != 98 || d2 != 99)
 				{
-					if ((d3 == d1 && d4 == d2) || (d3 == d2 && d4 == d1))
-						continue;
-					putchar(d1);
-					putchar(d2);
-					putchar(' ');
-					putchar(d3);
-					putchar(d4);
-					if (d1 == 57 && d2 == 56 && d3 == 57 && d4 == 57)
-						continue;
 					putchar(',');
 					putchar(' ');
 				}
+			}
+			d2++;
+		}
+		d1++;
+	}
 	putchar('\n');
 	return (0);
 }
