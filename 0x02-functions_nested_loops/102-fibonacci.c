2267a90711ea1cff9e5cb1e8c873d1d5a146b7c4
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-03-22 16:30:20 -0700

    task 14 has been done!

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, n = 50, t1 = 1, t2 = 2, nextTerm;
+
+	printf("Fibonacci Series: \n");
+	for (i = 1; i <= n; ++i)
+	{
+		printf("%d ", t1);
+		nextTerm = t1 + t2;
+		t1 = t2;
+		t2 = nextTerm;
+		printf("\n");
+	}
+	return (0);
+}
