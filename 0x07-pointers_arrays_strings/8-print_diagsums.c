4faa78d85af2b1ef907aae369a79f0aa5f1ee74a
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-04-03 18:02:09 -0700

    task 7 has been edited

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,18 +6,17 @@
  * @a: ptr to an array of integers
  * @size: size of array
  */
-
 void print_diagsums(int *a, int size)
 {
 	int i, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i < size * size; i += size + 1)
+	for (i = 0; i < size; i++)
 	{
-		sum1 += *(a + i);
+		sum1 += *(a + (i * size + i));
 	}
-	for (i = 0; i < (size * size) - 1; i += size - 1)
+	for (i = size - 1; i >= 0; i--)
 	{
-		sum2 += *(a + i);
+		sum2 += *(a + (i * size + (size - i - 1)));
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
