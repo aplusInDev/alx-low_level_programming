c94935023f0f22cc6737c4ee2174233441589c2a
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-04-03 17:15:55 -0700

    task 1 has benn edited

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,9 +10,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	char *csrc = (char *)src;
+	char *cdest = (char *)dest;
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		*(dest++) = *(src++);
+	{
+		cdest[i] = csrc[i];
+	}
 	return (dest);
 }
