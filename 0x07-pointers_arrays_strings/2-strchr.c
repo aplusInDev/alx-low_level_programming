fd8053da516270bd7bb11f1158dd867fefb6c267
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-04-03 17:09:27 -0700

    task 2 has benn edited

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s != (char)c)
 	{
-		if (*s == c)
-			return (s);
+		if (*s == '\0')
+		{
+			return (NULL);
+		}
 		s++;
 	}
-	return (NULL);
+	return ((char *)s);
 }
