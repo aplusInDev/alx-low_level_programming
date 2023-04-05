2d2311a63864b737975b06e8bb4626af68e0d6f7
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-04-05 10:46:09 -0700

    all do

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,37 @@
+#include <string.h>
+#include "main.h"
+/**
+ * wildcmp - task 9
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 1 if the strings can be considered identical, otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0')
+		{
+			return (1);
+		}
+		while (*s1 != '\0')
+		{
+			if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			{
+				return (1);
+			}
+			s1++;
+		}
+		return (0);
+	}
+	if (*s1 == '\0' || *s2 == '\0')
+	{
+		return ((*s1 == *s2));
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
