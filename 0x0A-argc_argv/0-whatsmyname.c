8d3efc2b2f130d63f87ef1f175f5913dff60b270
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-04-08 11:41:05 -0700

    task 0 done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "_putchar.c"
+#include "main.h"
+/**
+ * main - task 0
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 (success)
+ */
+int main(int argc[], char *argv[])
+{
+	int i = 0;
+
+	while (*(argv[0] + i) != '\0')
+	{
+		_putchar(*(argv[0] + i));
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
