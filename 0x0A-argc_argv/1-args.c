bab01498ff2dcef7a992a56b1de6d04ea4efeed9
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-04-08 11:57:24 -0700

    task 1 done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - task 0
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
