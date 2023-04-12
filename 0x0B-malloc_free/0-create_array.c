45fe4eb44628f9aa94889f16d63412175c185e52
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-04-12 14:33:16 -0700

    task 0 done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,11 +8,12 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *n = malloc(size);
+	char *arr = malloc(size * sizeof(char));
+	unsigned int i;
 
-	if (size == 0 || n == 0)
-		return (0);
-	while (size--)
-		n[size] = c;
-	return (n);
+	if (size == 0 || arr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+	return (arr);
 }
