558633b99815ff08dad3db77c51ad7af28ffd683
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-04-12 15:25:04 -0700

    task 1 done

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,12 +8,17 @@
  */
 char *_strdup(char *str)
 {
-	char *cp = malloc(sizeof(str));
-	int i;
+	char *dup_str;
+	unsigned int len = 0, i;
 
-	if (str == NULL || cp == NULL)
+	if (str == NULL)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-		cp[i] = str[i];
-	return (cp);
+	while (str[len])
+		len++;
+	dup_str = malloc((len + 1) * sizeof(char));
+	if (dup_str == NULL)
+		return (NULL);
+	for (i = 0; i <= len; i++)
+		dup_str[i] = str[i];
+	return (dup_str);
 }
