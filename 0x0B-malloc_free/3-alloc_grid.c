e1a30320314968bd071a4c9fb72c3f402eae4624
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-04-12 17:20:21 -0700

    task 2 done

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - task 3
+ * @width: arg1
+ * @height: arg2
+ *
+ * Return: grid_alloc
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid_alloc;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid_alloc = malloc(height * sizeof(int *));
+	if (grid_alloc == NULL)
+		return (NULL);
+	for (i = 0; i < width; i++)
+	{
+		grid_alloc[i] = malloc(width * sizeof(int *));
+		if (grid_alloc[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid_alloc[j]);
+			free(grid_alloc);
+			return (NULL);
+		}
+		for (j = 0; j < height; j++)
+			grid_alloc[i][j] = 0;
+	}
+	return (grid_alloc);
+}
