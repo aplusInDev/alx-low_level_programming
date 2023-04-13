d594620bfe38d4b61a07ffcf8e0c851d33bc5adc
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-04-13 09:47:37 -0700

    task 1 done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concat_str;
 	unsigned int len1 = 0, len2 = 0;
-	unsigned int i;
+	unsigned int i, j;
 
 	if (s1 == NULL)
 		s1 = "";
