f7c4a0bce80d63842a2d3f2dde88e342e37a108c
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-04-17 18:02:29 -0700

    task 4 has updated

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,41 @@
 #include <string.h>
 #include <stdlib.h>
 /**
+ * _memcpy - task 4
+ * @dest: distination
+ * @src: source
+ * @n: size
+ *
+ * Return: nothing
+ */
+void *_memcpy(void *dest, const void *src, size_t n)
+{
+	char *d = (char *)dest;
+	const char *s = (const char *)src;
+
+	while (n--)
+		*d++ = *s++;
+	return (dest);
+}
+/**
+ * my_strdup - task 4
+ * @src: string source
+ *
+ * Return: string
+ */
+char *my_strdup(const char *src)
+{
+	size_t len = strlen(src) + 1;
+	char *dst = malloc(len);
+
+	if (dst == NULL)
+	{
+		return (NULL);
+	}
+	_memcpy(dst, src, len);
+	return (dst);
+}
+/**
  * new_dog - copy dog info
  * @name: dog's name
  * @age: dog's age
@@ -17,14 +52,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	new_dog->name = strdup(name);
+	new_dog->name = my_strdup(name);
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
 	new_dog->age = age;
-	new_dog->owner = strdup(owner);
+	new_dog->owner = my_strdup(owner);
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
