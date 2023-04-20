065fd56775717e35b24b420347fedab4f28202b8
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-04-20 07:23:07 -0700

    task 3 done

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+/**
+ * main - check the code
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 (success)
+ */
+int main(int ac, char *av[])
+{
+	int (*op_fn)(int, int), a, b;
+
+	if (ac != 4)
+	{
+		printf("Error\n"), exit(98);
+	}
+	a = atoi(av[1]);
+	b = atoi(av[3]);
+	op_fn = get_op_func(av[2]);
+	if (!op_fn)
+	{
+		printf("Error\n"), exit(99);
+	}
+	if (!b && (av[2][0] == '/' || av[2][0] == '%'))
+	{
+		printf("Error\n"), exit(100);
+	}
+	printf("%d\n", op_fn(a, b));
+	return (0);
+}
