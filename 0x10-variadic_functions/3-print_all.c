ae46ac2e2db410a9a761b1a8e7ff38641d72e3a8
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-04-20 08:57:22 -0700

    task 3 done

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,87 @@
+#include "variadic_functions.h"
+
+/**
+ * format_char - formats character
+ * @separator: string separator
+ * @arg: arg pointer
+ *
+ * Return: nothing
+ */
+void format_char(char *separator, va_list arg)
+{
+	printf("%s%c", separator, va_arg(arg, int));
+}
+/**
+ * format_integer - formats int
+ * @separator: string separator
+ * @arg: arg pointer
+ *
+ * Return: nothing
+ */
+void format_integer(char *separator, va_list arg)
+{
+	printf("%s%d", separator, va_arg(arg, int));
+}
+/**
+ * format_float - formats float
+ * @separator: string separator
+ * @arg: arg pointer
+ *
+ * Return: nothing
+ */
+void format_float(char *separator, va_list arg)
+{
+	printf("%s%f", separator, va_arg(arg, double));
+}
+/**
+ * format_string - formats string
+ * @separator: string separator
+ * @arg: arg pointer
+ *
+ * Return: nothing
+ */
+void format_string(char *separator, va_list arg)
+{
+	char *str = va_arg(arg, char*);
+
+	if ((int)(!str))
+		str = "(nil)";
+	printf("%s%s", separator, str);
+}
+/**
+ * print_all - prints anything
+ * @format: type of arguments passed to the function
+ *
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0, j;
+	char *separator = "";
+	va_list arg;
+	token_t tokens[] = {
+		{"c", format_char},
+		{"i", format_int},
+		{"f", format_float},
+		{"s", format_string},
+		{NULL, NULL}
+	};
+
+	va_start(arg, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (tokens[j].t)
+		{
+			if (format[i] == token[j].t[0])
+			{
+				tokens[j].f(separator, arg);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(arg);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,17 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+
+/**
+ * struct token - struct token
+ * @t: format token
+ * @f: function
+ */
+typedef struct token
+{
+	char *t;
+	void (*f)(char *, va_list);
+} token_t;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
