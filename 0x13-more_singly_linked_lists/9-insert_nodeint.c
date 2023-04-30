4d11c0be49c69bc07f388d3edc23b97162fce401
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-04-30 15:42:10 +0000

    task 9 has updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -47,6 +47,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		return (new);
 	}
+	free(new);
 	return (NULL);
 }
 /**
@@ -59,6 +60,8 @@ listint_t *create_node(int n)
 {
 	listint_t *new = (listint_t *) malloc(sizeof(listint_t));
 
+	if (!new)
+		return (NULL);
 	new->n = n;
 	new->next = NULL;
 	return (new);
