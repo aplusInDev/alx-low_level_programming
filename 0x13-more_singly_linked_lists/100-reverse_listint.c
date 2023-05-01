13ade8888a7a19290fe2450c3e0ab0a8aabe587e
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-05-01 18:09:33 +0000

    task 4 done

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a listint_t linked list
+ * @head: the listint_t root
+ *
+ * Return: new root
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *tmp, *root;
+
+	/*root = (listint_t *) malloc(sizeof(listint_t));*/
+	root = NULL;
+	if (!(*head))
+		return (NULL);
+	while (*head)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		tmp->next = root;
+		root = tmp;
+	}
+	*head = root;
+	return (*head);
+}
