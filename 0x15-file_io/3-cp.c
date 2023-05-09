a9e11053c34850b88c503f71b8e8c8758d1bcc7e
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-05-09 01:13:40 +0000

    updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,27 +4,27 @@
 
 /**
  * main - copies the content of a file to another file
- * @argc: arg count
- * @argv: arg vector
+ * @ac: arg count
+ * @av: arg vector
  *
  * Return: Return: 0 on success, exit with error code otherwise
  */
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
 	int file_from, file_to;
 	ssize_t bytes_read, bytes_written;
 	char buffer[BUFFER_SIZE];
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
-	if (argc != 3)
+	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	file_from = open(argv[1], O_RDONLY);
+	file_from = open(av[1], O_RDONLY);
 	if (file_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		close(file_from), exit(99);
 	}
 	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
@@ -32,13 +32,13 @@ int main(int argc, char **argv)
 		bytes_written = write(file_to, buffer, bytes_read);
 		if (bytes_written != bytes_read)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			close(file_from), close(file_to), exit(99);
 		}
 	}
 	if (bytes_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		close(file_from), close(file_to), exit(98);
 	}
 	if (close(file_from) == -1)
